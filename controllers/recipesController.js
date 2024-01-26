8bdd7468b3842f6185928a45724458aff8874e6f
Ilona <lona.krechko@gmail.com>
2024-01-26 11:19:42 +0000

    Added error handling

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const createError = require("http-errors");
 const Recipe = require("../models/recipe");
 
-exports.getRecipesData = async (req, res) => {
+exports.getRecipesData = async (req, res, next) => {
   const options = {
     method: "GET",
     url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
@@ -22,11 +22,11 @@ exports.getRecipesData = async (req, res) => {
     const response = await axios.request(options);
     res.send(response.data.results);
   } catch (error) {
-    console.error(error);
+    return next(createError(404, error.message));
   }
 };
 
-exports.addOneFavorite = async (req, res) => {
+exports.addOneFavorite = async (req, res, next) => {
   const options = {
     method: "GET",
     url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${req.params.id}/information`,
@@ -38,6 +38,7 @@ exports.addOneFavorite = async (req, res) => {
 
   try {
     const response = await axios.request(options);
+
     const favorite = new Recipe({
       title: response.data.title,
       image: response.data.image,
@@ -47,34 +48,71 @@ exports.addOneFavorite = async (req, res) => {
     await favorite.save();
     res.send(favorite);
   } catch (error) {
-    console.error(error);
+    return next(createError(404, error.message));
   }
 };
 
-exports.displayFavorites = async (req, res) => {
-  const favorites = await Recipe.find();
-  res.send(favorites);
+exports.displayFavorites = async (req, res, next) => {
+  try {
+    const favorites = await Recipe.find();
+    res.send(favorites);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
 
 exports.deleteFavorite = async (req, res, next) => {
-  const favoriteId = req.params.id;
-  const deletedFavorite = await Recipe.findByIdAndDelete(favoriteId);
-  res.send({ result: true });
+  try {
+    const favoriteId = req.params.id;
+    const deletedFavorite = await Recipe.findByIdAndDelete(favoriteId);
+
+    if (!deletedFavorite) {
+      return next(createError(404, "No favorite recipe with that id"));
+    }
+
+    res.send({ result: true });
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
 
-exports.addComments = async (req, res) => {
-  const recipeId = req.params.id;
-  const favorite = await Recipe.findById(recipeId);
-  favorite.comments.push({ text: req.body.text });
-  await favorite.save();
-  res.send(favorite);
+exports.addComments = async (req, res, next) => {
+  try {
+    const recipeId = req.params.id;
+    const favorite = await Recipe.findById(recipeId);
+
+    if (!favorite) {
+      return next(createError(404, "No favorite recipe with that id"));
+    }
+
+    favorite.comments.push({ text: req.body.text });
+    await favorite.save();
+    res.send(favorite);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
 
-exports.changeComment = async (req, res) => {
-  const commentId = req.params.commentId;
-  const favoriteId = req.params.favoriteId;
-  const favorite = await Recipe.findById(favoriteId);
-  favorite.comments.id(commentId).text = req.body.text;
-  await favorite.save();
-  res.send(favorite);
+exports.changeComment = async (req, res, next) => {
+  try {
+    const commentId = req.params.commentId;
+    const favoriteId = req.params.favoriteId;
+    const favorite = await Recipe.findById(favoriteId);
+
+    if (!favorite) {
+      return next(createError(404, "No favorite with that id"));
+    }
+
+    const favoriteComment = favorite.comments.id(commentId);
+
+    if (!favoriteComment) {
+      return next(createError(404, "No favorite comment with that id"));
+    }
+
+    favoriteComment.text = req.body.text;
+    await favorite.save();
+    res.send(favorite);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
