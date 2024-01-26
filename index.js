74704ec0600f5ab5b5a3ee71ba643ae3adcf06f6
Ilona <lona.krechko@gmail.com>
2024-01-26 10:36:12 +0000

    Main page fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(router);
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Welcome to Gluten Free Recipes API!</h1> <p>Enjoy the following features:</p><ul><li>'/all' - Get 10 gluten recipes recepies from RapidAPI</li><li>'/favorites' - Get the list of your chosen favorite recipes</li><li>'/favorites/:id(example - 189909, 608074)' - Add a new recipe to your favorites</li><li>'/favorites/:id(189909, 608074)' - Remove a recipe from your list by :id</li><li>'/favorites/:id(example - 189909, 608074)/comments' - Add a comment to a specific recipe</li><li>'/favorites/:favoriteId(example - 189909, 608074)/comments/:commentId' - Modify a specific comment</li></ul>"
+    "<h1>Welcome to Gluten Free Recipes API!</h1> <p>Enjoy the following features:</p><ul><li>'/all' - Get 10 gluten recipes from RapidAPI</li><li>'/favorites' - Get the list of your chosen favorite recipes</li><li>'/favorites/:id(example - 189909, 608074)' - Add a new recipe to your favorites</li><li>'/favorites/:id(189909, 608074)' - Remove a recipe from your list by :id</li><li>'/favorites/:id/comments' - Add a comment to a specific recipe</li><li>'/favorites/:favoriteId(example - 189909, 608074)/comments/:commentId' - Modify a specific comment</li></ul>"
   );
 });
 
